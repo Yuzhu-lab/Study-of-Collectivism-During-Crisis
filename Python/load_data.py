94ab9d3fbf9eb5bc2fa34183ec713ce007118e65
Yuzhu-lab <68797511+Yuzhu-lab@users.noreply.github.com>
2020-08-12 12:34:58 -0400

    load HRS sas dataset

---
+++ Python/load_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data_00 = pd.read_sas(r'C:\Users\Mirach\Desktop\Yuzhu\Research_Assistant\Collectivism\h00f1c_SAS\h00f1c.sas7bdat')
+data_02 = pd.read_sas(r'C:\Users\Mirach\Desktop\Yuzhu\Research_Assistant\Collectivism\h02f2c_SAS\h02f2c.sas7bdat')
+data_04 = pd.read_sas(r'C:\Users\Mirach\Desktop\Yuzhu\Research_Assistant\Collectivism\h04f1c_SAS\h04f1c.sas7bdat')
+data_06 = pd.read_sas(r'C:\Users\Mirach\Desktop\Yuzhu\Research_Assistant\Collectivism\h06f3a_SAS\h06f3a.sas7bdat')
+data_08 = pd.read_sas(r'C:\Users\Mirach\Desktop\Yuzhu\Research_Assistant\Collectivism\h08f3a_SAS\h08f3a.sas7bdat')
+data_10 = pd.read_sas(r'C:\Users\Mirach\Desktop\Yuzhu\Research_Assistant\Collectivism\hd10f5e_SAS\hd10f5e.sas7bdat')
+data_12 = pd.read_sas(r'C:\Users\Mirach\Desktop\Yuzhu\Research_Assistant\Collectivism\h12f2a_SAS\h12f2a.sas7bdat')
+data_14 = pd.read_sas(r'C:\Users\Mirach\Desktop\Yuzhu\Research_Assistant\Collectivism\h14f2a_SAS\h14f2a.sas7bdat')
+data_16 = pd.read_sas(r'C:\Users\Mirach\Desktop\Yuzhu\Research_Assistant\Collectivism\h16f2a_SAS\h16f2a.sas7bdat')
